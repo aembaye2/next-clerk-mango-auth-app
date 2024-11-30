b126093ba3e8f9612c89ec4c5692f53e037caca5
Abel Embaye <aembaye@uark.edu>
2024-11-30 01:11:55 -0600

    commited 11/29/2024h

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -12,7 +12,7 @@ export const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "next-clerk-mango-auth-proj",
+      dbName: "next-auth-app",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
