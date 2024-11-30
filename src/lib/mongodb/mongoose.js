19f203e057a3f077d63dff8f7b8bd3ee1c38642d
Abel Embaye <aembaye@uark.edu>
2024-11-30 00:26:32 -0600

    commited 11/29/2024e

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,24 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let initialized = false;
 
 export const connect = async () => {
-  mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
   if (initialized) {
-    console.log('MongoDB already connected');
+    console.log("MongoDB already connected");
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'next-auth-app',
+      dbName: "next-clerk-mangodb-auth-proj",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
     initialized = true;
   } catch (error) {
-    console.log('MongoDB connection error:', error);
+    console.log("MongoDB connection error:", error);
   }
 };
